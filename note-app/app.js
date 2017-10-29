d17037076938139cc2480b0d4f42914686eecfe0
Brendan Koral <brendanhkoral@gmail.com>
2017-10-29 16:54:28 -0500

    Add app.js with Lodash require

--- note-app/app.js
+++ note-app/app.js
@@ -3,16 +3,21 @@ console.log('Starting app')
 const fs = require('fs')
 const os = require('os')
 const notes = require('./notes')
+const _ = require('lodash')
 
 let res = notes.addNote()
 
-console.log(res)
+// console.log(_.isString(true))
+// console.log(_.isString('Koral'))
+
+let filteredArray = _.uniq(['Koral', 'Koral', 28])
+console.log(filteredArray)
 
 // let user = os.userInfo()
 
 
 // fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function (err) {
 //   if (err) {
-//       console.log('File could not be written to.')
+//       console.log('File could not be written to.' + err)
 //     }
 // })
