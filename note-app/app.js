4a7bafe3be231dbd3088e66dd1e7229872bfeed6
Brendan Koral <brendanhkoral@gmail.com>
2017-10-29 16:28:02 -0500

    Add notes.js

--- note-app/app.js
+++ note-app/app.js
@@ -1,6 +1,13 @@
 console.log('Starting app')
 
 const fs = require('fs')
+const os = require('os')
 
-fs.appendFile('greetings.txt', 'Hello world!')
+let user = os.userInfo()
 
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err) {
+  if (err) {
+      console.log('File could not be written to.')
+    }
+})
