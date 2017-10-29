25281a709c4ade6a7a8a91a4c2a21fefb0e29809
Brendan Koral <brendanhkoral@gmail.com>
2017-10-29 16:39:15 -0500

    Add require for notes file with an exported function

--- note-app/app.js
+++ note-app/app.js
@@ -2,12 +2,17 @@ console.log('Starting app')
 
 const fs = require('fs')
 const os = require('os')
+const notes = require('./notes')
 
-let user = os.userInfo()
+let res = notes.addNote()
+
+console.log(res)
+
+// let user = os.userInfo()
 
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err) {
-  if (err) {
-      console.log('File could not be written to.')
-    }
-})
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function (err) {
+//   if (err) {
+//       console.log('File could not be written to.')
+//     }
+// })
