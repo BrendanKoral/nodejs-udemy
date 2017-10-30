df27b16368ce2e81d270907af36e34840a758428
Brendan Koral <brendanhkoral@gmail.com>
2017-10-29 20:13:25 -0500

    Add setup for all commands for note app

--- note-app/app.js
+++ note-app/app.js
@@ -3,19 +3,24 @@ console.log('Starting app')
 const fs = require('fs')
 const notes = require('./notes')
 const _ = require('lodash')
+const yargs = require('yargs')
+
+const argv = yargs.argv
 
 let command = process.argv[2]
 
+// console.log('Command', command)
+// console.log(argv)
+
 if (command === 'add') {
-  console.log('Adding new note')
+  notes.addNote(argv.title, argv.body)
 } else if (command === 'list') {
-  console.log('Listing all notes')
+  notes.getAll()
 } else if (command === 'read') {
-  console.log('Reading note')
+  notes.getNote(argv.title)
 } else if (command === 'remove') {
-  console.log('Removing note')
-}
-else {
+  notes.removeNote(argv.title)
+} else {
   console.log('Command not recognized')
 }
 
