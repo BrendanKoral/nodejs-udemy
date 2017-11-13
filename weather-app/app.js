4210ab043a0cd5f4b46fac44e3044b9a649e82c4
Brendan Koral <brendanhkoral@gmail.com>
2017-11-12 20:37:53 -0600

    Add initial argv functionality

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,27 @@
 const request = require('request')
+const yargs = require('yargs')
+
+const argv = yargs
+.options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address for which to fetch weather',
+        string: true
+    }
+})
+.help()
+.alias('help', 'h')
+.argv
+
+let encodedAddress = encodeURIComponent(argv.a)
+
+console.log(argv)
+
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=10%20E%20Main%20Street,%20Roselle,%20IL%2060172',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    console.log(body)
+    console.log(JSON.stringify(body, undefined, 2))
 })
