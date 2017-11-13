4cd8f3fd6643585ce7ab890a2a24c8fb596248b0
Brendan Koral <brendanhkoral@gmail.com>
2017-11-12 22:56:08 -0600

    Add completed outline for error handling + success feedback

--- weather-app/app.js
+++ weather-app/app.js
@@ -23,5 +23,16 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
+
+    if (error) {
+        console.log('Unable to connect to Google services.')
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('There were no matches for that address.')
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`)
+        console.log(`Latitude: ${body.results[0].geometry.location.lng}`)
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+    }
+
     console.log(JSON.stringify(body, undefined, 2))
 })
