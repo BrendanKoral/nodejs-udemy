980482528dbba31c35760efc8ac938b3b4be3ac4
Brendan Koral <brendanhkoral@gmail.com>
2017-11-13 07:55:45 -0600

    Add geocode.js in order to refactor

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,3 @@
-const request = require('request')
 const yargs = require('yargs')
 
 const argv = yargs
@@ -14,25 +13,6 @@ const argv = yargs
 .alias('help', 'h')
 .argv
 
-let encodedAddress = encodeURIComponent(argv.a)
-
 console.log(argv)
 
 
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-}, (error, response, body) => {
-
-    if (error) {
-        console.log('Unable to connect to Google services.')
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('There were no matches for that address.')
-    } else if (body.status === 'OK') {
-        console.log(`Address: ${body.results[0].formatted_address}`)
-        console.log(`Latitude: ${body.results[0].geometry.location.lng}`)
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
-    }
-
-    console.log(JSON.stringify(body, undefined, 2))
-})
