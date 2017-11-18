3ec2f2baf9368c062d1103d6c828ec62933bcfa6
Brendan Koral <brendanhkoral@gmail.com>
2017-11-17 20:31:08 -0600

    Add Handlebars, a couple of basic routes

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,23 +1,24 @@
 const express = require('express')
+const hbs = require('hbs')
 
 let app = express()
 
+app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
 
 app.get('/',(req, res) => {
-    // res.send('<h1>Hello Express!</h1>')
-    res.send({
-        name: 'Brendan',
-        likes: [
-            'motorcycles',
-            'bass',
-            'Traveling'
-        ]
-    })
+   res.render('home.hbs', {
+       pageTitle: 'Homepage',
+       welcomeMessage: 'Hello world, welcome to this random site/route!',
+       currentYear: new Date().getFullYear()
+   })
 })
 
 app.get('/about', (req, res) => {
-    res.send('About Page')
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    })
 })
 
 app.get('/bad', (req, res) => {
@@ -26,6 +27,6 @@ app.get('/bad', (req, res) => {
     })
 })
 
-app.listen(3000)
-
-console.log('Listening on port 3000...')
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
