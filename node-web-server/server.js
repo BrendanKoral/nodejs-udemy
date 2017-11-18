07ea97b116fa1ce6815970da70d0fe5937c3e208
Brendan Koral <brendanhkoral@gmail.com>
2017-11-17 21:16:29 -0600

    Add some middleware practice stuff that was actually pretty cool

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,36 @@
 const express = require('express')
 const hbs = require('hbs')
+const fs = require('fs')
 
 let app = express()
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+
+    let now = new Date().toString()
+
+    let log = `${now}: ${req.method} | ${req.url}`
+
+    console.log(log)
+
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server log.')
+        }
+    })
+
+    //MUST call this or else the rest of the code won't fire
+    next()
+})
+
+//Can show a maintenance page using middleware and just prevent the server from routing anywhere else
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs')
+// })
+
 app.use(express.static(__dirname + '/public'))
 
 hbs.registerHelper('getCurrentYear', () => {
