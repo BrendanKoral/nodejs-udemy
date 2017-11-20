cf2a1548878aa52f2462dd6173ab34d634d7dc74
Brendan Koral <bkoral@dealerinspire.com>
2017-11-19 20:49:12 -0600

    Add basic assertion testing

--- writing-tests/utils/utils.test.js
+++ writing-tests/utils/utils.test.js
@@ -1,19 +1,48 @@
 //Add watcher for tests: nodemon --exec "npm test"
 
+const expect = require('expect')
+
 const utils = require('./utils')
 
 it('should add two numbers together', () => {
     let res = utils.add(5, 10)
 
-    if ( res !== 15 ) {
-        throw new Error(`Result was not correct. Expected 15, was: ${res}`)
-    }
+    expect(res).toBe(15).toBeA('number')
+
 })
 
 it('should square a number', () => {
     let result = utils.square(5)
 
-    if (result !== 25) {
-        throw new Error(`Result was incorrect. Expected 25, was: ${result}`)
-    }
+    expect(result).toBe(25).toBeA('number')
+
+    //If you want to compare objects, you can't use toBe because triple equals is going to check for two of the same object
+    //We won't get that because they're 2 objects at 2 different memory locations with the same properties, therefor different
+    //Instead, use toEqual or notToEqual
+
+    //Other example:
+
+    /*
+    expect({
+        name: "Brendan",
+        age: 28,
+        location: "Chicago"
+    }).toInclude({
+        age: 28
+    })
+    */
+
+})
+
+it('should return my full name', () => {
+
+    let user = {location: 'Chicago', age: 25}
+    let res = utils.setName(user, 'Brendan Koral')
+
+    expect(user).toEqual(res)
+
+    expect(res).toInclude({
+        firstName: 'Brendan',
+        lastName: 'Koral'
+    })
 })
