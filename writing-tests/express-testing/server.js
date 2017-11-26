75c2478b9ba620736e392996340d490b6276ab78
Brendan Koral <brendanhkoral@gmail.com>
2017-11-26 10:13:14 -0600

    Add testing for dummy data using a couple of routes

--- writing-tests/express-testing/server.js
+++ writing-tests/express-testing/server.js
@@ -3,9 +3,27 @@ const express = require('express')
 let app = express()
 
 app.get('/', (req,res) => {
-    res.send('Hello world!')
+    res.status(404).send({
+        error: 'Page not found.',
+        name: 'Todo App v1.0'
+    })
+})
+
+app.get('/users', (req, res) => {
+    res.status(200).send([
+        {
+            name: 'Brendan',
+            age: 28
+        },
+        {
+            name: 'Wes',
+            age: 25
+        }
+    ])
 })
 
 app.listen(3000, () => {
     console.log('App now listening on port 3000')
 })
+
+module.exports.app = app
