b97f509830bddca30c2297429ceed6cc4c7f33b0
Brendan Koral <bkoral@dealerinspire.com>
2017-11-27 20:53:42 -0600

    Add testing with spies and rewire

---
+++ writing-tests/spies/app.test.js
@@ -0,0 +1,29 @@
+const expect = require('expect')
+const rewire = require('rewire')
+
+let app = rewire('./app.js')
+
+describe('App', () => {
+
+    let db = {
+        saveUser: expect.createSpy()
+    }
+
+    app.__set__('db', db)
+
+    it('should call the spy correctly', () => {
+        let spy = expect.createSpy()
+
+        spy()
+        expect(spy).toHaveBeenCalled()
+    })
+
+    it('should call saveUser with user object', () => {
+        let email = 'koral@example.com'
+        let password = '123abc'
+
+        app.handleSignup(email, password)
+
+        expect(db.saveUser).toHaveBeenCalledWith({email, password})
+    })
+})
