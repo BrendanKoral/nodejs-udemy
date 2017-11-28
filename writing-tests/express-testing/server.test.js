05a0ac5f34fa1e7cd62166b3b1a195941d3fb6fa
Brendan Koral <bkoral@dealerinspire.com>
2017-11-27 20:33:48 -0600

    Add describe blocks for testing, including nested blocks

--- writing-tests/express-testing/server.test.js
+++ writing-tests/express-testing/server.test.js
@@ -3,27 +3,33 @@ const expect = require('expect')
 
 const app = require('./server').app
 
-it('should return hello world as a string for a response', (done) => {
-    request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-        expect(res.body).toInclude({
-            error: 'Page not found.'
+describe('Server', () => {
+    describe('GET /', () => {
+        it('should return hello world as a string for a response', (done) => {
+            request(app)
+            .get('/')
+            .expect(404)
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    error: 'Page not found.'
+                })
+            })
+            .end(done)
         })
     })
-    .end(done)
-})
-
-it('should return some dummy data that contains my name and age', (done) => {
-    request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-        expect(res.body).toInclude({
-            name: 'Brendan',
-            age: 28
+    describe('GET /users', () => {
+        it('should return some dummy data that contains my name and age', (done) => {
+            request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    name: 'Brendan',
+                    age: 28
+                })
+            })
+            .end(done)
         })
     })
-    .end(done)
+    
 })
--- writing-tests/utils/utils.test.js
+++ writing-tests/utils/utils.test.js
@@ -4,49 +4,53 @@ const expect = require('expect')
 
 const utils = require('./utils')
 
-it('should add two numbers together', () => {
-    let res = utils.add(5, 10)
+describe('Utils', () => {
 
-    expect(res).toBe(15).toBeA('number')
-
-})
-
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4,3,(sum) => {
-        expect(sum).toBe(7).toBeA('number')
+    it('should add two numbers together', () => {
+        let res = utils.add(5, 10)
+    
+        expect(res).toBe(15).toBeA('number')
+    
+    })
+    
+    it('should async add two numbers', (done) => {
+        utils.asyncAdd(4,3,(sum) => {
+            expect(sum).toBe(7).toBeA('number')
+            done()
+        })
+    })
+    
+    it('should async square a number', (done) => {
+        utils.asyncSquare(5, (res) => {
+            expect(res).toBe(25).toBeA('number')
+        })
         done()
     })
-})
-
-it('should async square a number', (done) => {
-    utils.asyncSquare(5, (res) => {
-        expect(res).toBe(25).toBeA('number')
+    
+    it('should square a number', () => {
+        let result = utils.square(5)
+    
+        expect(result).toBe(25).toBeA('number')
+    
+        //If you want to compare objects, you can't use toBe because triple equals is going to check for two of the same object
+        //We won't get that because they're 2 objects at 2 different memory locations with the same properties, therefor different
+        //Instead, use toEqual or notToEqual
+    
+        //Other example:
+    
+        /*
+        expect({
+            name: "Brendan",
+            age: 28,
+            location: "Chicago"
+        }).toInclude({
+            age: 28
+        })
+        */
+    
     })
-    done()
 })
 
-it('should square a number', () => {
-    let result = utils.square(5)
-
-    expect(result).toBe(25).toBeA('number')
-
-    //If you want to compare objects, you can't use toBe because triple equals is going to check for two of the same object
-    //We won't get that because they're 2 objects at 2 different memory locations with the same properties, therefor different
-    //Instead, use toEqual or notToEqual
-
-    //Other example:
-
-    /*
-    expect({
-        name: "Brendan",
-        age: 28,
-        location: "Chicago"
-    }).toInclude({
-        age: 28
-    })
-    */
-
-})
 
 it('should return my full name', () => {
 
