9b5df5867d8808d0ce2d382d2a2a9c5d8b07a3c4
Brendan Koral <brendanhkoral@gmail.com>
2017-11-29 15:10:34 -0600

    Add show count of Todos

--- todo-api/playground/mongodb-find.js
+++ todo-api/playground/mongodb-find.js
@@ -17,7 +17,23 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
     console.log('Connected to MongoDB server')
 
-    
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5a1f1ed777c7118414313404')
+    // }).toArray().then((docs) => {
+    //     console.log(`Todos:`)
+    //     console.log(JSON.stringify(docs, undefined, 2))
+    // }, (err) => {
+    //     console.error(`Unable to fetch todos. ${err}`)
+    // })
+
+
+db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos count: ${count}`)
+        
+    }, (err) => {
+        console.error(`Unable to fetch todos. ${err}`)
+    })
+
    
-    db.close()
+    // db.close()
 })
