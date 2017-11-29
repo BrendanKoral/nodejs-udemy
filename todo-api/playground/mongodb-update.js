91605e9e79fdd6be232b68522afe56c8506a71da
Brendan Koral <brendanhkoral@gmail.com>
2017-11-29 16:28:54 -0600

    Add update lesson

--- todo-api/playground/mongodb-update.js
+++ todo-api/playground/mongodb-update.js
@@ -9,7 +9,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
     console.log('Connected to MongoDB server')
 
-   
+   db.collection('Todos').findOneAndUpdate( {
+        _id: new ObjectID('5a1f1827b1de6e1dd8d14fa9')
+   }, {
+       $set: {
+           completed: true
+       }
+   }, {
+       returnOriginal: false
+   })
+   .then((result) => {
+       console.log(result)
+   })
    
     // db.close()
 })
