a005429ddc1ccf52f8f1b68151a5ea54d4d0cc32
Brendan Koral <brendanhkoral@gmail.com>
2017-11-29 14:53:41 -0600

    Add some destructuring practice, ObjectID stuff

--- todo-api/playground/mongodb-connect.js
+++ todo-api/playground/mongodb-connect.js
@@ -1,4 +1,14 @@
-const MongoClient = require('mongodb').MongoClient
+// const MongoClient = require('mongodb').MongoClient
+
+const {MongoClient, ObjectID} = require('mongodb')
+
+//ObjectID lets you make IDs on the fly
+
+let obj = new ObjectID()
+
+console.log(obj)
+
+
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
@@ -7,16 +17,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
     console.log('Connected to MongoDB server')
 
-    db.collection('Todos').insertOne({
-        text: 'Something to do',
-        completed: false
-    }, (err, res)=> {
-        if (err) {
-            return console.error(`Unable to insert todo: ${err}`)
-        }
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, res)=> {
+    //     if (err) {
+    //         return console.error(`Unable to insert todo: ${err}`)
+    //     }
 
-        console.log(JSON.stringify(res.ops, undefined, 2))
-    })
+    //     console.log(JSON.stringify(res.ops[0]._id.getTimestamp(), undefined, 2))
+    // })
 
     db.close()
 })
