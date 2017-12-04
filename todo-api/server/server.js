3211d35e8870c40830c9cfd6290b62c1f25ebd9b
Brendan Koral <brendanhkoral@gmail.com>
2017-12-03 19:41:28 -0600

    Add basic Todo and Mongoose connection

--- todo-api/server/server.js
+++ todo-api/server/server.js
@@ -1,5 +1,9 @@
 let mongoose = require('mongoose');
+/*
+    Have to CD on Windows to C:\Program Files\MongoDB\Server\3.4\bin
 
+    ./mongod.exe --dbpath /Users/Koral/mongo-data
+*/
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost:27017');
@@ -21,6 +25,18 @@ let newTodo = new Todo({
 })
 
 newTodo.save().then((doc) => {
+    console.log(`Successfully saved todo! ${doc}`);
+}, (err) => {
+    console.error('Unable to save todo');
+})
+
+let otherTodo = new Todo({
+    text: 'Walk the dog',
+    completed: true,
+    completedAt: 123
+})
+
+otherTodo.save().then((doc) => {
     console.log(`Successfully saved todo! ${doc}`);
 }, (err) => {
     console.error('Unable to save todo');
