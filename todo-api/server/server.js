10c80393a8595e72ca5e93c5a8ff0d7c672d4511
Brendan Koral <bkoral@dealerinspire.com>
2017-12-05 15:26:11 -0600

    Add todos/:id route

--- todo-api/server/server.js
+++ todo-api/server/server.js
@@ -1,9 +1,10 @@
-let express = require('express');
-let bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-let { mongoose } = require('./db/mongoose');
-let {Todo} = require('./models/todo');
-let {User} = require('./models/user');
+const { mongoose } = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 /*
     Have to CD on Windows to C:\Program Files\MongoDB\Server\3.4\bin
@@ -34,6 +35,30 @@ app.get('/todos', (req,res) => {
         });
     }, (err) => res.status(400).send(err))
 });
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send()
+        }
+    
+        res.status(200).send({todo})
+
+    }).catch((e) => res.status(404).send())
+
+
+    //Else query DB and find by ID. Include success and error
+    //On E - send 400 and send empty body back .send()
+    
+    //On S - send it back
+    //On No Todo - send 404 with empty body
+})
 
 app.listen(3000, () => {
     console.log(`Todo API started on port 3000`);
