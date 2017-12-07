d60909217e81a6587bc856c6cc44e7c7b380fd9d
Brendan Koral <bkoral@dealerinspire.com>
2017-12-06 21:54:35 -0600

    Add environment variables for test, dev, and prod

--- todo-api/server/db/mongoose.js
+++ todo-api/server/db/mongoose.js
@@ -6,7 +6,7 @@ let mongoose = require('mongoose');
 */
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { 
     mongoose
