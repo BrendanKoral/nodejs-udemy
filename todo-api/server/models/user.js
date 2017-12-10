2741fca3a811e95d13834cc81e866a8e275c9c8a
Brendan Koral <brendanhkoral@gmail.com>
2017-12-10 14:19:30 -0600

    Add user model setup

--- todo-api/server/models/user.js
+++ todo-api/server/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const jwt = require('jsonwebtoken')
 
-let User = mongoose.model('User', {
+let UserSchema = new mongoose.Schema({
     email : {
         type: String,
         required: true,
@@ -29,5 +30,21 @@ let User = mongoose.model('User', {
         }
     }]  
 })
+
+UserSchema.methods.generateAuthToken = function () {
+    let user = this
+
+    let access = 'auth'
+
+    let token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString()
+
+    user.tokens.push({access, token})
+
+    user.save().then(() => {
+        return token
+    })
+}
+
+let User = mongoose.model('User', UserSchema)
 
 module.exports = {User}
